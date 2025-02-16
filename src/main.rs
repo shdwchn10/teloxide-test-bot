6f2e80c70c9b701f392da8db0cfbaf9d7734cc7e
Andrey Brusnik <dev@shdwchn.io>
2025-02-16 20:50:45 +0400

    Test is_*_deactivated

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use teloxide::{
-    prelude::*, sugar::request::RequestReplyExt, types::ParseMode, utils::markdown, RequestError,
+    adaptors::{throttle::Limits, trace::Settings, CacheMe, Throttle, Trace},
+    prelude::*,
+    sugar::bot::BotMessagesExt,
+    RequestError,
 };
 
 #[tokio::main]
@@ -8,7 +11,10 @@ async fn main() {
 
     pretty_env_logger::init();
 
-    let bot = Bot::from_env();
+    let bot: Trace<Throttle<CacheMe<Bot>>> = Bot::from_env()
+        .cache_me()
+        .throttle(Limits::default())
+        .trace(Settings::all());
 
     let handler = dptree::entry()
         .inspect(|u: Update| {
@@ -16,11 +22,18 @@ async fn main() {
         })
         .branch(
             Update::filter_message().branch(Message::filter_text().endpoint(
-                |msg: Message, bot: Bot| async move {
-                    bot.send_message(msg.chat.id, markdown::blockquote(msg.text().unwrap()))
-                        .reply_to(msg.id)
-                        .parse_mode(ParseMode::MarkdownV2)
-                        .await?;
+                |msg: Message, bot: Trace<Throttle<CacheMe<Bot>>>| async move {
+                    let user_id = if let Ok(user_id) = msg.text().unwrap_or_default().parse::<u64>()
+                    {
+                        UserId(user_id)
+                    } else {
+                        return Ok(());
+                    };
+
+                    if bot.is_user_deactivated(user_id).await {
+                        bot.send_message(msg.chat.id, "User is deactivated!")
+                            .await?;
+                    }
 
                     Ok::<(), RequestError>(())
                 },
